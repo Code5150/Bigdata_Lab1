e1b21cf81099c468babe899b4dde3a3c965cee8b
Code5150 <f>
2022-01-06 20:31:00 +0400

    Fixes and report

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -3,7 +3,7 @@ package com.vladislav
 import org.apache.log4j.{Level, Logger}
 import org.apache.spark._
 
-import java.time.LocalDateTime
+import java.time.{LocalDateTime, ZoneOffset}
 import java.time.format.DateTimeFormatter
 
 case class Station(stationId: Int, name: String, lat: Double, long: Double, dockcount: Int,
@@ -57,29 +57,32 @@ object Main {
       )
     )
 
+    val bikeWithMaxRun = tripsInternal.map(x => (x.bikeId, x.duration)).reduceByKey(_ + _)
+      .max()(Ordering.by[(Int, Int), Int](_._2))
+    println("Bike with max run id: " + bikeWithMaxRun._1 + ", run: " + bikeWithMaxRun._2)
+
     val distancesBetweenStations = sc.broadcast(stationsInternal.cartesian(stationsInternal).map(sp =>
       ((sp._1.stationId, sp._2.stationId), distanceBetween(sp._1, sp._2))
     ).collect().toMap)
 
-    val biggestDistanceBetweenStations = distancesBetweenStations.value.max(Ordering.by[((Int, Int), Double), Double](_._2))
-    println("Biggest distance between stations " + biggestDistanceBetweenStations._1._1
-      + " and " + biggestDistanceBetweenStations._1._2 + ": " + biggestDistanceBetweenStations._2)
-
-    val bikeWithMaxRun = tripsInternal.map(x => (x.bikeId, distancesBetweenStations.value(
-      (x.startStationId, x.endStationId)
-    ))).reduceByKey(_ + _).max()(Ordering.by[(Int, Double), Double](_._2))
-    println("Bike with max run id: " + bikeWithMaxRun._1, " run: " + bikeWithMaxRun._2)
+    val bikes = tripsInternal.map(trip => trip.bikeId).distinct().count()
+    println("Bikes count: " + bikes)
 
     val bikesWithMoreThan3Hours = tripsInternal.keyBy(rec => rec.bikeId)
       .mapValues(x => x.duration)
       .reduceByKey(_ + _)
       .filter(p => p._2 > 10800)
-
     println("Bikes with duration more than 3 hours: " + bikesWithMoreThan3Hours.count())
     //bikesWithMoreThan3Hours.foreach(println)
 
-    val bikes = tripsInternal.map(trip => trip.bikeId).distinct().count()
-    println("Bikes count: " + bikes)
+    val biggestDistanceBetweenStations = distancesBetweenStations.value.max(Ordering.by[((Int, Int), Double), Double](_._2))
+    println("Biggest distance between stations " + biggestDistanceBetweenStations._1._1
+      + " and " + biggestDistanceBetweenStations._1._2 + ": " + biggestDistanceBetweenStations._2)
+
+    val bikeWithMaxRunPath = tripsInternal.filter(t => t.bikeId == bikeWithMaxRun._1)
+      .sortBy(_.startDate.toInstant(ZoneOffset.UTC).toEpochMilli)
+    println("Bike with max run path:")
+    bikeWithMaxRunPath.foreach(println)
 
     sc.stop()
   }
